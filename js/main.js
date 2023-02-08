a03cf3187c191fdeb72dd6198386ca42bc09f42d
Saurabh Mishra <saurabh@simformsolutions.com>
2023-02-08 17:57:53 +0530

    update all feature

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ var data;
 var sortArr = [];
 //defult data show
 getStoreData(0)
-
+//form validation
 function validateform(id) {
     getFormValue();
     var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.jfif|\.gif)$/i;
@@ -21,8 +21,7 @@ function validateform(id) {
     else if (proName == null || proName == "") {
         document.getElementById('errorMessageName').innerText = "Please input a product name!!";
     }
-    else if(!allowedExtensions.exec(proImage))
-    {
+    else if (!allowedExtensions.exec(proImage)) {
         document.getElementById('errorMessageImage').innerText = "File type is not allowed!!";
         proImage.value = '';
         return false;
@@ -31,13 +30,12 @@ function validateform(id) {
         document.getElementById('errorMessagePrice').innerText = "Please input a number only!!";
     }
     else {
-        if(id=='add')
-        {
+        if (id == 'add') {
             formData();
+            alert('Product added successfull!!');
         }
-        else
-        {
-            setEditdata(); 
+        else {
+            setEditdata();
         }
     }
 }
@@ -75,9 +73,29 @@ function storeData() {
         btnValueSet(getVal);
     }
 }
+function validate() { 
+    getVal = btnValueGet();
+    proId = document.getElementById('productId').value;
+    proName = document.getElementById('ProductName').value;
+    for (let i = 0; i < getVal; i++) {
+        pro = "productValue" + i;
+        var data = JSON.parse(localStorage.getItem(pro));
+        console.log(data[0]);
+        if(proId == data[0] || proName == data[1])
+        {
+            let msg = "Product ID and Name should be unique"; 
+            alert(msg);
+            return false; 
+        }
+    }
+}
 //store data function
 function getStoreData(dataStore) {
     var table = document.getElementById("proDetails");
+    getVal = btnValueGet();
+    let price;
+    var sortVal;
+    sortVal = getVal-1;
     if (dataStore == 'ser') { //search condition by search button
         var serData = document.getElementById('dataFilter').value;
         let ser = "productValue" + serData;
@@ -89,10 +107,11 @@ function getStoreData(dataStore) {
                 cell.innerHTML = "<img src='" + 'img\\' + data[j] + "' width='60em'/>";
             }
             else if (j == 3) {
-                cell.innerHTML = "₹ " + data[j];
+                price = Number(data[j]).toFixed(2)
+                cell.innerHTML = "₹ " + price;
             }
             else if (j == 5) {
-                cell.innerHTML = "<button class='btn btn-primary' onclick='productUpdate(this.id)' id='" + ser + "'>Edit</button> <button class='btn btn-dander' onclick='productDelete(this.id)' id='" + ser + "'>Delete</button>";
+                cell.innerHTML = "<button class='btn btn-primary' onclick='productUpdate(this.id)' id='" + ser + "'>Edit</button> <button class='btn btn-danger' onclick='productDelete(this.id)' id='" + ser + "'>Delete</button>";
             }
             else {
                 cell.innerHTML = data[j];
@@ -106,30 +125,22 @@ function getStoreData(dataStore) {
         }
     }
     else if (dataStore == 'pName') { //search condition by product name
-        getVal = btnValueGet();
         for (let i = 0; i < getVal; i++) {
-            pro = "productValue" + i;
-            var data = JSON.parse(localStorage.getItem(pro));
-            sortArr.push(data[1]);
-        }
-        sortArr.sort()
-        console.log(sortArr.sort());
-        var sortIndex = 0;
-        for (let i = 0; i < getVal; i++) {
-            pro = "productValue" + i;
+            pro = "productValue" + sortVal;
+            sortVal--;
             var row = table.insertRow(i);
             var data = JSON.parse(localStorage.getItem(pro));
             for (let j = 0; j < data.length + 1; j++) {
                 var cell = row.insertCell(j);
                 if (j == 1) {
-                    cell.innerHTML = sortArr[sortIndex];
-                    sortIndex++;
+                    cell.innerHTML = data[j];
                 }
                 else if (j == 2) {
                     cell.innerHTML = "<img src='" + 'img\\' + data[j] + "' width='60em'/>";
                 }
                 else if (j == 3) {
-                    cell.innerHTML = "₹ " + data[j];
+                    price = Number(data[j]).toFixed(2)
+                    cell.innerHTML = "₹ " + price;
                 }
                 else if (j == 5) {
                     cell.innerHTML = "<button class='btn btn-primary' onclick='productUpdate(this.id)' id='" + pro + "'>Edit</button> <button class='btn btn-danger' onclick='productDelete(this.id)' id='" + pro + "'>Delete</button>";
@@ -141,37 +152,25 @@ function getStoreData(dataStore) {
         }
         var tbodyRowCount = table.rows.length;
         for (let i = 0; i < tbodyRowCount; i++) {
-            if (i > 2) {
+            if (i > getVal-1) {
                 table.rows[i].style.display = "none";
             }
         }
     }
     else if (dataStore == 'pricePro') { //search condition by product price
-        getVal = btnValueGet();
         for (let i = 0; i < getVal; i++) {
-            pro = "productValue" + i;
-            var row = table.insertRow(i);
-            var data = JSON.parse(localStorage.getItem(pro));
-            sortArr.push(data[3]);
-        }
-        sortArr.sort()
-        console.log(sortArr.sort());
-        var sortIndex = 0;
-        for (let i = 0; i < getVal; i++) {
-            pro = "productValue" + i;
+            pro = "productValue" + sortVal;
+            sortVal--;
             var row = table.insertRow(i);
             var data = JSON.parse(localStorage.getItem(pro));
             for (let j = 0; j < data.length + 1; j++) {
                 var cell = row.insertCell(j);
                 if (j == 3) {
-                    cell.innerHTML = sortArr[sortIndex];
-                    sortIndex++;
+                    price = Number(data[j]).toFixed(2)
+                    cell.innerHTML = "₹ " + price;
                 }
                 else if (j == 2) {
                     cell.innerHTML = "<img src='" + 'img\\' + data[j] + "' width='60em'/>";
-                }
-                else if (j == 3) {
-                    cell.innerHTML = "₹ " + data[j];
                 }
                 else if (j == 5) {
                     cell.innerHTML = "<button class='btn btn-primary' onclick='productUpdate(this.id)' id='" + pro + "'>Edit</button> <button class='btn btn-danger' onclick='productDelete(this.id)' id='" + pro + "'>Delete</button>";
@@ -183,7 +182,7 @@ function getStoreData(dataStore) {
         }
         var tbodyRowCount = table.rows.length;
         for (let i = 0; i < tbodyRowCount; i++) {
-            if (i > 2) {
+            if (i > getVal - 1) {
                 table.rows[i].style.display = "none";
             }
         }
@@ -200,7 +199,8 @@ function getStoreData(dataStore) {
                     cell.innerHTML = "<img src='" + 'img\\' + data[j] + "' width='60em'/>";
                 }
                 else if (j == 3) {
-                    cell.innerHTML = "₹ " + data[j];
+                    price = Number(data[j]).toFixed(2)
+                    cell.innerHTML = "₹ " + price;
                 }
                 else if (j == 5) {
                     cell.innerHTML = "<button class='btn btn-primary' onclick='productUpdate(this.id)' id='" + pro + "'>Edit</button> <button class='btn btn-danger' onclick='productDelete(this.id)' id='" + pro + "'>Delete</button>";
@@ -223,6 +223,7 @@ function productDelete(pro) {
     }
 }
 //product update function
+var editpro;
 function productUpdate(pro) {
     document.getElementById("btnSubmit").style.display = "none";
     document.getElementById("btnUpdate").style.display = "block";
@@ -231,9 +232,7 @@ function productUpdate(pro) {
         data = JSON.parse(localStorage.getItem(pro));
     }
     document.getElementById('productId').readOnly = true;
-    editData();
-}
-function editData() {
+    editpro = pro;
     getEditData();
 }
 //get edited data
@@ -244,26 +243,17 @@ function getEditData() {
     proPrice = document.getElementById('price').value = data[3];
     proDecpt = document.getElementById('description').value = data[4];
 }
-function changeImage()
-{
+function changeImage() {
     proImage = document.getElementById('image').files[0].name;
     document.getElementById("showImage").innerHTML = "<img src='" + 'img\\' + proImage + "' width='120em'/>";
 }
-//update edited data
-function updatedData() {
-    proId = document.getElementById('productId').value;
-    proName = document.getElementById('ProductName').value;
-    proImage = document.getElementById('image').files[0].name;
-    proPrice = document.getElementById('price').value;
-    proDecpt = document.getElementById('description').value;
-}
 //store edited data
 function setEditdata() {
-    updatedData();
+    getFormValue();
     createArray();
-    if (pro) {
+    if (editpro) {
         if (confirm("You want to edit your data!")) {
-            localStorage.setItem(pro, JSON.stringify(proArray));
+            localStorage.setItem(editpro, JSON.stringify(proArray));
         }
     }
 }
